6883aa04d5142a01f0db63211ac8ac551030ac48
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-04-06 13:52:29 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import AddTransaction from './components/AddTransaction';
+import Balance from './components/Balance';
+import ExpenseList from './components/ExpenseList';
+import Header from './components/Header'
+import IncomeList from './components/IncomeList';
+// import { GlobalContextProvider } from './context/GlobalState';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        // <GlobalContextProvider>
+            <div className='container'>
+                <div className="app-wrapper">
+                    <Header />
+                    <Balance />
+                    <AddTransaction />
+                    <IncomeList />
+                    <ExpenseList />
+                </div>
+            </div>
+        // </GlobalContextProvider>
+    );
+};
 
 export default App;
---
+++ src/context/GlobalState.js
@@ -0,0 +1,33 @@
+import React, {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer';
+
+
+const initialState = {
+    incomeTransactions: [
+        { id:1, incomeText: "Car sold", incomeAmount: 15000 },
+        { id:2, incomeText: "Salary", incomeAmount: 5000 },
+        { id:3, incomeText: "Bonus", incomeAmount: 13000 }
+    ],
+    expenseTransactions: [
+        { id:4, expenseText: "Rent", expenseAmount: 1000 },
+        { id:5, expenseText: "Bank", expenseAmount: 2000 },
+        { id:6, expenseText: "Clothes", expenseAmount: 500 }
+    ]
+};
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalContextProvider = ({children}) => {
+    const [ state, dispatch ] = useReducer( AppReducer, initialState )
+
+    return (
+        <GlobalContextProvider 
+        value={{
+            incomeTransactions : state.incomeTransactions,
+            expenseTransactions: state.expenseTransactions
+        }}>
+        {children}
+        </GlobalContextProvider>
+    )
+}
+
